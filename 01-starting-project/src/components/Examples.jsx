6e2bfce2a561af6db34867f44e3b980322f409a3
Johna210 <solomonjohna21@gmail.com>
2024-07-03 18:07:02 +0300

    Added more components and split bigger components and refactor some code

---
+++ 01-starting-project/src/components/Examples.jsx
@@ -0,0 +1,67 @@
+import { useState, Fragment } from "react";
+import TabButton from "./TabButton.jsx";
+import Section from "./Section.jsx";
+import Tabs from "./Tabs.jsx";
+import { EXAMPLES } from "../data";
+
+export default function Examples() {
+  // Always have 2 elements
+  // const stateArray = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleClick(selectedItem) {
+    setSelectedTopic(selectedItem);
+    console.log(selectedTopic);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onClick={() => handleClick("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onClick={() => handleClick("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onClick={() => handleClick("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onClick={() => handleClick("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {tabContent}
+      </Tabs>
+    </Section>
+  );
+}
