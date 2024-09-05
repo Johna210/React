d35e89939be7d726527f3ff650d64d0972bd5740
Johna210 <solomonjohna21@gmail.com>
2024-09-05 11:41:49 +0300

    simple project app

---
+++ 06-practice-project/src/components/NewTask.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import Button from './Button';
+
+export default function NewTask({ onAddTask }) {
+  const [enteredTask, setEnteredTask] = useState('');
+
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+
+  function handleClick() {
+    if (enteredTask.trim() === '') {
+      return;
+    }
+    onAddTask(enteredTask);
+    setEnteredTask('');
+  }
+
+  return (
+    <div className="flex items-center gap-4">
+      <input
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        onChange={handleChange}
+        value={enteredTask}
+      />
+      <Button onClick={handleClick}>Add Task</Button>
+    </div>
+  );
+}
